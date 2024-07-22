d6b53942f3178796af714be739070cb74e0f3572
majdza24-meet <zahermajd3@gmail.com>
2024-07-22 23:36:24 +0300

    database lab

--- Auth lab/app.py
+++ Auth lab/app.py
@@ -8,29 +8,33 @@ Config = {
   "storageBucket": "auth-lab-1451a.appspot.com",
   "messagingSenderId": "414219816042",
   "appId": "1:414219816042:web:41e631f3510c99498e0e13",
-  "databaseURL":""
+"databaseURL":"https://auth-lab-1451a-default-rtdb.europe-west1.firebasedatabase.app/"
 
 }
 firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
-
+db = firebase.database()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods = ['GET', 'POST'])
 def signUp():
   if request.method == 'POST':
+    full_name = request.form['full_name']
+    username = request.form['username']
     email = request.form['email']
     password = request.form['password']
     try:
-      print("hello")
+      user = {'email' : email, 'username': username, 'full_name': full_name}
       session['user'] = auth.create_user_with_email_and_password(email, password)
-      session['quote'] = ""
-      session['quotes'] = []
+      uid = session['user']['localId']
+      db.child('Users').child(uid).set(user)
       return redirect(url_for('home'))
     except:
       error = "Authentication failed"
+      return redirect(url_for("error"))
   return render_template("signup.html")
+
 
 
 @app.route('/sign-in', methods=['GET','POST'])
@@ -40,41 +44,47 @@ def signIn():
     password = request.form['password']
     try:
       session['user'] = auth.sign_in_with_email_and_password(email, password)
-      session['quote'] = ""
-      session['quotezs'] = []
       return redirect(url_for('home'))
     except:
       error = "Authentication failed"
+      return redirect(url_for("error"))
   return render_template("signin.html")
-
-@app.route('/home', methods = ['GET','POST'])
-def home():
-  if request.method == 'POST':
-    print(session['quote'])
-    quote = request.form['quote']
-    session['quote'] = quote
-    return redirect(url_for('thanks'))
-  return render_template('home.html')
 
 @app.route('/display')
 def display():
-  quotes = session['quotes']
-  print(session['quotes'])
-  return render_template("display.html", quotes = quotes)\
+  if session['user'] != None:
+    return render_template("display.html", quotes = db.child('Quotes').get().val())
+  else:
+    return redirect(url_for('signIn'))
+@app.route('/home', methods = ['GET','POST'])
+def home():
+  if request.method == 'POST':
+    quote = request.form['quote']
+    speaker = request.form['speaker']
+    session['speaker'] = speaker
+    session['quote'] = quote
+    quote_info= {'said_by' : speaker,'quote': quote, 'uid' : session['user']['localId']}
+    db.child('Quotes').push(quote_info)
+    return redirect(url_for('thanks'))
+  return render_template('home.html')
 
 @app.route('/thanks')
 def thanks():
-  quote = session['quote']
-  session['quotes'].append(quote)
-  session.modified = True
-  print(session['quotes'])
-  return render_template("thanks.html",quote = quote)
+  if session['user'] != None:
+    quote = session['quote']
+    speaker = session['speaker']
+    return render_template("thanks.html",quote = quote, speaker = speaker)
+  else:
+    return redirect(url_for('signIn'))
 
 @app.route('/sign-out')
 def signOut():
   session['user']=None
   auth.current_user = None
   return redirect(url_for('signIn'))
+@app.route('/error.html')
+def error():
+  return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
